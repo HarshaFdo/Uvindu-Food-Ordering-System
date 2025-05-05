45b665c6ad8df003ae5d8128b4aeaf507f664380
Harsha Fernando <116500252+HarshaFdo@users.noreply.github.com>
2025-05-05 21:47:55 +0530

    Revert "Sachintha"

--- foodcabin_backend/meals/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Meal
-
-class MealSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Meal
-        fields = '__all__'

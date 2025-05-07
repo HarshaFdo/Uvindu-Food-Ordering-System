903406a832316b9c9bb619ef2c580a045b8a1e64
Roshan-Silva <roshan0122silva@gmail.com>
2025-05-08 01:21:21 +0530

    Admin dashboard with basic functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import UserPage from './pages/UserPage';
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/user" element={<UserPage/>}/>
+        <Route path="/admin" element={<AdminDashboard />}/>
       </Routes>
     </BrowserRouter>
   );
